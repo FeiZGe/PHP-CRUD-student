829a84ba27807f80f1e62e0b1fb82c949b0cffec
FeiZGe <thanapattt.m@gmail.com>
2024-09-15 15:21:40 +0700

    link action btn

---
+++ src/components/actionbtn.php
@@ -0,0 +1,11 @@
+<div class="flex flex-wrap sm:flex-nowrap gap-1">
+    <a href="./details.php" class="btn btn-info btn-square btn-sm text-base-100 text-lg tooltip tooltip-right sm:tooltip-top tooltip-info hover:scale-110" data-tip="รายละเอียด" onclick="my_modal_sorry.showModal()">
+        <i class="fa-solid fa-circle-info"></i>
+    </a>
+    <button class="btn btn-warning btn-square btn-sm text-base-100 text-lg tooltip tooltip-right sm:tooltip-top tooltip-warning hover:scale-110" data-tip="แก้ไขข้อมูล" onclick="my_modal_sorry.showModal()">
+        <i class="fa-solid fa-pen"></i>
+    </button>
+    <button class="btn btn-error btn-square btn-sm text-base-100 text-lg tooltip tooltip-right sm:tooltip-top tooltip-error hover:scale-110" data-tip="ลบข้อมูล" onclick="my_modal_sorry.showModal()">
+        <i class="fa-solid fa-trash"></i>
+    </button>
+</div>
--- src/master.php
+++ src/master.php
@@ -315,17 +315,7 @@ require_once "../config/dbconnect.php";
 
                                     <!-- Action -->
                                     <td>
-                                        <div class="flex flex-wrap sm:flex-nowrap gap-1">
-                                            <a href="./detail.html" class="btn btn-info btn-square btn-sm text-base-100 text-lg tooltip tooltip-right sm:tooltip-top tooltip-info hover:scale-110" data-tip="รายละเอียด" onclick="my_modal_sorry.showModal()">
-                                                <i class="fa-solid fa-circle-info"></i>
-                                            </a>
-                                            <button class="btn btn-warning btn-square btn-sm text-base-100 text-lg tooltip tooltip-right sm:tooltip-top tooltip-warning hover:scale-110" data-tip="แก้ไขข้อมูล" onclick="my_modal_sorry.showModal()">
-                                                <i class="fa-solid fa-pen"></i>
-                                            </button>
-                                            <button class="btn btn-error btn-square btn-sm text-base-100 text-lg tooltip tooltip-right sm:tooltip-top tooltip-error hover:scale-110" data-tip="ลบข้อมูล" onclick="my_modal_sorry.showModal()">
-                                                <i class="fa-solid fa-trash"></i>
-                                            </button>
-                                        </div>
+                                        <?php include('./components/actionbtn.php') ?>
                                     </td>
                                 </tr>
                                 
